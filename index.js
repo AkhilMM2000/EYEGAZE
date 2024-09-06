e3210dea8b3579add5577ab8bb187a5eeece1b46
Akhil <manojakhil36@gmail.com>
2024-09-06 18:44:34 +0530

    remove the log

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.use('/admin',adminRoute)
 
 //port for host
 const port=process.env.port||3500
-console.log(port);
 
 app.listen(port,()=>{
     console.log(`server running http://localhost:${port} `);
