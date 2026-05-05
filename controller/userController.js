77c9b89cc4492c845231159eb01d3ce0c7a646c1
Akhil <manojakhil36@gmail.com>
2026-05-05 20:30:55 +0530

    forget passoword link change

--- controller/userController.js
+++ controller/userController.js
@@ -245,7 +245,7 @@ const forget_password = async (req, res) => {
 const new_password = async (req, res) => {
   try {
     const get_email = req.body.email;
-    const link = `https://eyegaze.shop/forgetpassword`;
+    const link = `https://sarathipari.shop/forgetpassword`;
     const check_mail = await User.findOne({ userEmail: get_email });
 
     if (!check_mail) {
